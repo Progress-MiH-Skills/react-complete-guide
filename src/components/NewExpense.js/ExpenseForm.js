377352e7949931ddac5c440c6242a49cde37358d
Nick Eric Kayumba <kericox@gmail.com>
2022-11-24 06:39:40 +0200

    Updating that depends on the previous state

--- src/components/NewExpense.js/ExpenseForm.js
+++ src/components/NewExpense.js/ExpenseForm.js
@@ -3,6 +3,14 @@ import { useState } from 'react';
 import './NewExpense.css';
 
 const ExpenseForm = () => {
+
+  // Multiple States approach (preferred) - useState() 
+  //returns an array with 2 elements - the current state snapshot and a 
+  //function to update the state snapshot (setState()) - the function is 
+  //called with the new state snapshot as an argument - the function is called by 
+  //React when the component is rendered - the function is called by React when the 
+  //component is re-rendered - the function is called by React when the component is unmounted
+
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
@@ -17,6 +25,50 @@ const ExpenseForm = () => {
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+  // One State approach - useState() returns an array with 2 elements - 
+  // the current state snapshot and a function to update the state snapshot (setState()) - 
+  // the function is called with the new state snapshot as an argument - the function is 
+  // called by React when the component is rendered - the function is called by React when 
+  // the component is re-rendered - the function is called by React when the component is unmounted 
+
+
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
+
+  // const titleChangeHandler = (event) => {
+  //   setUserInput({
+  //     ...userInput, // spread operator to keep the previous state values and only update the changed value below (enteredTitle)
+  //     enteredTitle: event.target.value, // update the changed value (enteredTitle) only and keep the previous state values (enteredAmount and enteredDate) as they are (see spread operator above)
+  //   });
+  // };
+
+  // const amountChangeHandler = (event) => {
+  //   setUserInput({
+  //     ...userInput, // spread operator
+  //     enteredAmount: event.target.value,
+  //   });
+  // };
+
+  // const dateChangeHandler = (event) => {
+  //   setUserInput({
+  //     ...userInput, // spread operator
+  //     enteredDate: event.target.value,
+  //   });
+
+  // // when depending on the previous state, use the function syntax below
+  // const dateChangeHandler = (event) => {
+  //   setUserInput((prevState) => {
+  //     return { ...prevState, enteredDate: event.target.value };
+  //   });
+
   };
 
   return (
