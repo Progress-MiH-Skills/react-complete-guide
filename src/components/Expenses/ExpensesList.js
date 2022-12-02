1829a633f79a8397fa98722d6c4bb770de4c0468
Nick Eric Kayumba <kericox@gmail.com>
2022-12-02 13:18:49 +0200

    refactoring List in a separate files

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+import ExpenseItem from "./ExpenseItem";
+
+import "./ExpensesList.css";
+
+const ExpensesList = (props) =>   {let expensesContent = <p>No expenses found</p>
+if (props.filteredExpenses.length > 0) { 
+  expensesContent = props.filteredExpenses.map((item) => 
+  <ExpenseItem 
+    key={item.id}
+    title={item.title} 
+    amount={item.amount} 
+    date={item.date} 
+  />)}
+  return <ul className="expenses-list">{expensesContent}</ul>
+}
+
+export default ExpensesList;
