96db755c07b682035822fd33264fa2575d6522d2
Nick Eric Kayumba <50874777+kericox@users.noreply.github.com>
2022-12-02 12:53:12 +0200

    Outputing conditional content Refined (#8)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,22 +10,27 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
   // filter is a built-in array method that takes a function as an argument and returns a new array 
-  // with the elements that pass the test implemented by the provided function 
+  // with the elements that pass the test implemented by the provided function
   const filteredExpenses = props.expenses.filter((expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   }));
+  
+  let expensesContent = <p>No expenses found</p>
+  if (filteredExpenses.length > 0) { 
+    expensesContent = filteredExpenses.map((item) => 
+    <ExpenseItem 
+      key={item.id}
+      title={item.title} 
+      amount={item.amount} 
+      date={item.date} 
+    />)}
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter filteredYear={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((item) => 
-          <ExpenseItem 
-            key={item.id}
-            title={item.title} 
-            amount={item.amount} 
-            date={item.date} 
-          />)}
+        {expensesContent}
 
       </Card>
     </div>
