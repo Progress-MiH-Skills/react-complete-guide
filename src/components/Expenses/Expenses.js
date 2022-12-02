00e7687ab90b94d17c3ede057e740be59a2a4ce1
Nick Eric Kayumba <kericox@gmail.com>
2022-12-02 13:18:49 +0200

    refactoring List in a separate files

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { React, useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from '../Filters/ExpensesFilter';
@@ -17,21 +17,12 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   }));
   
-  let expensesContent = <p>No expenses found</p>
-  if (filteredExpenses.length > 0) { 
-    expensesContent = filteredExpenses.map((item) => 
-    <ExpenseItem 
-      key={item.id}
-      title={item.title} 
-      amount={item.amount} 
-      date={item.date} 
-    />)}
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter filteredYear={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
-
+        <ExpensesList filteredExpenses={filteredExpenses} />
       </Card>
     </div>
   );
