f2c213b6baf49d15a2294c07854f3b08646f3602
Nick Eric Kayumba <kericox@gmail.com>
2022-12-03 04:38:01 +0200

    Outputing conditional content with conditin in return

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,19 +1,22 @@
 import React from "react";
-
 import ExpenseItem from "./ExpenseItem";
-
 import "./ExpensesList.css";
 
-const ExpensesList = (props) =>   {let expensesContent = <p>No expenses found</p>
-if (props.filteredExpenses.length > 0) { 
-  expensesContent = props.filteredExpenses.map((item) => 
-  <ExpenseItem 
-    key={item.id}
-    title={item.title} 
-    amount={item.amount} 
-    date={item.date} 
-  />)}
-  return <ul className="expenses-list">{expensesContent}</ul>
+const ExpensesList = (props) =>   {
+  if (props.filteredExpenses.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">{props.filteredExpenses.map((item) => 
+      <ExpenseItem 
+        key={item.id}
+        title={item.title} 
+        amount={item.amount} 
+        date={item.date} 
+      />)}</ul>  
+  );
+
 }
 
 export default ExpensesList;
